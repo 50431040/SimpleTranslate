53fca05cf4b2d95fb5a8216ee67297f7cf923015
Stahsf <50431040@qq.com>
2024-05-18 16:04:10 +0800

    Initial Commit

---
+++ SimpleTranslate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleTranslate
+//
+//  Created by Stahsf on 2024/5/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleTranslate/SimpleTranslateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTranslateApp.swift
+//  SimpleTranslate
+//
+//  Created by Stahsf on 2024/5/18.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTranslateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
